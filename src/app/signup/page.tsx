26664940a813b19499f63d7a49beb834b3a8a638
V1ncentNg <112238267+V1ncentNg@users.noreply.github.com>
2024-06-13 22:26:46 +1000

    add a signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import SignupForm from "./signupForm";
+
+export default function SignupPage() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <SignupForm />
+    </div>
+  );
+}
