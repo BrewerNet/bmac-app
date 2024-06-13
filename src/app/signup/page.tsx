baf18026e71defcbe2e0092a969575b46e3dcf61
V1ncentNg <112238267+V1ncentNg@users.noreply.github.com>
2024-06-13 22:25:23 +1000

    restruct the signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,123 +1,12 @@
 'use client';
 
-import { useState } from "react";
-import axios from '../../utils/axios'
+import SignupForm from "./SignupForm";
 
-const SignUpPage = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    username: '',
-    first_name:'',
-    middle_name:'',
-    last_name:'',
-    password:'',
-    mobile_number:''
-  });
-
-  const handleSubmit = async (e:any) => {
-    e.preventDefault(); 
-    const signUpUrl = process.env.NEXT_PUBLIC_SIGNUP_URL;
-    if (!signUpUrl) {
-      console.error('SIGN_UP_URL environment variable is not defined');
-      return;
-    }
-    try {
-      const response = await axios.post(signUpUrl, formData);
-      if (response.status === 200) {
-        console.log('Form data submitted successfully!', response.data);
-      } else {
-        console.error('Failed to submit form data.');
-      }
-    } catch (error) {
-      console.error('Error submitting form data:', error);
-    }
-    console.log("Submit Value:", formData);
-  }
-
-  const handleChange = (e:any) => {
-    const { name, value } = e.target;
-    setFormData(prevFormData => ({
-      ...prevFormData,
-      [name]: value
-    }));
-  };
-
-  return(
+export default function SignupPage() {
+  return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <label>
-          First Name:
-          <input
-            type="text"
-            name="first_name"
-            value={formData.first_name}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <label>
-          Middle Name:
-          <input
-            type="text"
-            name="middle_name"
-            value={formData.middle_name}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <label>
-          Last Name:
-          <input
-            type="text"
-            name="last_name"
-            value={formData.last_name}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <label>
-          User Name:
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <label>
-          Mobile Number:
-          <input
-            type="text"
-            name="mobile_number"
-            value={formData.mobile_number}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+      <h1>Sign Up</h1>
+      <SignupForm />
     </div>
-  )
+  );
 }
-
-export default SignUpPage;
